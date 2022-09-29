1f5310a6b12fb74ee28987d6337a2038f2b936ac
Ahmed Hamam <ahmedhamam50@gmail.com>
2022-09-29 10:36:07 +0200

    Solve some easy problem

--- C#/CoderHub/Program.cs
+++ C#/CoderHub/Program.cs
@@ -1,1 +1,28 @@
-﻿System.Console.WriteLine(value: "arsasd");
+﻿namespace MyNamespace
+{
+    public class MyClass
+    {
+        public static void Main(string[] args)
+        {
+            // System.Console.WriteLine(is_same("Anas", "anas"));
+            System.Console.WriteLine(deleteLastChar("Ahmed"));
+        }
+        public static string date_format(string date)
+        {
+            // write your code here
+            var date_format = Convert.ToDateTime(date);
+            return date_format.ToString("yyyy/M/d") + " | " + date_format.ToString("yyyy-M-d") + " | " + date_format.ToString("M/d/yyyy");
+        }
+        public static string is_same(string name1, string name2)
+        {
+            // write your code here
+            return name1 == name2 ? "متشابهتين" : "غير متشابهتين";
+        }
+        public static string deleteLastChar(string word)
+        {
+            // write your code here
+            return word.Substring(0, word.Length - 1);
+
+        }
+    }
+}
