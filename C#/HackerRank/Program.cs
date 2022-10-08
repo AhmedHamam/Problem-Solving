b449244a7891a530b45637e1b95f39b1d3586d5a
Ahmed Hamam <ahmedhamam50@gmail.com>
2022-10-08 11:38:14 +0200

    Update Program.cs

--- C#/HackerRank/Program.cs
+++ C#/HackerRank/Program.cs
@@ -1,18 +1,4 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
-
-class Result
+﻿class Result
 {
 
     /*
@@ -43,11 +29,11 @@ class Solution
 {
     public static void Main(string[] args)
     {
-        double meal_cost = Convert.ToDouble(Console.ReadLine().Trim());
+        double meal_cost = Convert.ToDouble(Console.ReadLine()!.Trim());
 
-        int tip_percent = Convert.ToInt32(Console.ReadLine().Trim());
+        int tip_percent = Convert.ToInt32(Console.ReadLine()!.Trim());
 
-        int tax_percent = Convert.ToInt32(Console.ReadLine().Trim());
+        int tax_percent = Convert.ToInt32(Console.ReadLine()!.Trim());
 
         Result.solve(meal_cost, tip_percent, tax_percent);
     }
